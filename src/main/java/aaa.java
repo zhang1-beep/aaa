32de5cb507281ab9063abbca0bde0d5df2794d4a
瑞吉 <15170211+ri_gi@user.noreply.gitee.com>
2025-12-05 11:02:26 +0800

    update aaa.java

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -2,8 +2,8 @@ public class aaa {
     public static void main(String[] args) {
 
         System.out.println("a111aab111111");
-        System.out.println("aaabbbbsfdad");
-        System.out.println("aaaabbbbbbbbb);
+        System.out.println("aadbbsfdad");
+        System.out.println("aaaabfsfsfsbbbbb);
 
     }
 }
