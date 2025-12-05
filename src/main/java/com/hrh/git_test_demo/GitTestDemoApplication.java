2614d5b85ba60cad4dd2b3c5acf8704c3e2c67e6
瑞吉 <15170211+ri_gi@user.noreply.gitee.com>
2025-12-05 10:11:07 +0800

    add aaa.java

---
+++ src/main/java/com/hrh/git_test_demo/GitTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hrh.git_test_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestDemoApplication.class, args);
+	}
+
+}
