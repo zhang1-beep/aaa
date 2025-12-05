64653646ae8876a2e8cc433b93766728dd539d15
瑞吉 <15170211+ri_gi@user.noreply.gitee.com>
2025-12-05 11:02:07 +0800

    update aaa.java third

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -1,9 +1,7 @@
 public class aaa {
     public static void main(String[] args) {
-
         System.out.println("a111aab111111");
-        System.out.println("aaabbbbsfdad");
+        System.out.println("aa111111dad");
         System.out.println("aaaabbbbbbbbb);
-
     }
 }
